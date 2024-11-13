eecf141431bee2d2596ce38586ba20ee56679d55
Faruk D. <f.diblen@esciencecenter.nl>
2024-11-13 18:27:47 +0100

    rename metric as identifier

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -3,7 +3,7 @@ import json
 from pathlib import Path
 from jsonschema import validate
 
-def test_metric_schema():
-    schema = json.load(open("schemas/metric_schema.json"))
-    instance = json.load(open("examples/metric_example.json"))
+def test_identifier_json_schema():
+    schema = json.load(open("schemas/identifier_schema.json"))
+    instance = json.load(open("examples/identifier_example.json"))
     assert validate(instance=instance, schema=schema) is None
