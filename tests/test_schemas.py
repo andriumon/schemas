36e694ebcda87f1f818720db80bf67a10a76b1b7
Faruk D. <f.diblen@esciencecenter.nl>
2024-11-18 09:57:31 +0100

    rename the identifier schema as indicator schema

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -3,7 +3,7 @@ import json
 from pathlib import Path
 from jsonschema import validate
 
-def test_identifier_json_schema():
-    schema = json.load(open("schemas/identifier_schema.json"))
-    instance = json.load(open("examples/identifier_example.json"))
+def test_indicator_json_schema():
+    schema = json.load(open("schemas/indicator_schema.json"))
+    instance = json.load(open("examples/indicator_example.json"))
     assert validate(instance=instance, schema=schema) is None
